91bc605c835e8d2f3e687e6af51a175a67382063
neo <674292593@eqq.com>
2018-01-20 23:46:29 +0800

    Push from Linux!

---
+++ .ipynb_checkpoints/test_1-checkpoint.py
@@ -0,0 +1,14 @@
+import scineo as sn
+import numpy as np
+
+train,train_y,test,test_y = np.load("DataSet/No4.npy")
+train_x,test_x = train.reshape(-1,128*128),test.reshape(-1,128*128)
+X = np.concatenate([train_x,test_x])
+X = sn.pca(X)
+print(X.shape)
+train_x,test_x = X[:len(train_y)],X[len(train_y):]
+#sn.predict_knn(train_x,train_y,test_x,test_y,show=True)
+sn.predict(train_x,train_y,test_x,test_y,model = sn.model_svm,show=True)
+
+
+
